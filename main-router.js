d8d3a2187c5695469d3b38beddeaac6325352206
Razak <mohammedrazak2001@gmail.com>
2023-03-24 18:14:34 +0530

    added mongo session

--- main-router.js
+++ main-router.js
@@ -6,7 +6,25 @@ const passport = require("passport");
 router.get('/', function(req, res) {
     res.send('Hello, world!');
 });
-
+router.post('/registerUser', function(req, res) {
+    console.log(req.body);
+    const { name, email, password } = req.body;
+    const user = new User({
+        name: name,
+        email: email,
+        password: password
+    });
+    console.log(name, email, password); 
+    res.json(name, email, password);
+    // user.save()
+    //     .then(savedUser => {
+    //         res.json(savedUser);
+    //     })
+    //     .catch(err => {
+    //         console.error(err);
+    //         res.status(500).send('Error saving user');
+    //     });
+});
 router.get('/users', function(req, res, next) {
     User.find({})
       .exec()
